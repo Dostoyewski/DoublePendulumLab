040565fef22112f9f3e7094377e9523fa6de0d75
Fedor Kondratenko <dostoyewski@yandex.ru>
2022-04-25 17:06:16 +0300

    Move detector to class

--- main.py
+++ main.py
@@ -2,30 +2,39 @@ import cv2 as cv
 import numpy as np
 import pandas as pd
 
+
+class ARUCODetector(object):
+    def __init__(self, filename):
+        self.cap = cv.VideoCapture(filename)
+        self.res_df = pd.DataFrame(columns=['31x', '31y', '32x', '32y', '33x', '33y'])
+
+    def run(self):
+        while True:
+            success, image = self.cap.read()
+            if image is None:
+                break
+            key = cv.waitKey(1) & 0xFF
+            if (key == 27) or (key == ord('q')):
+                break
+            if success:
+                dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
+                parameters = cv.aruco.DetectorParameters_create()
+                markerCorners, markerIds, rejectedCandidates = cv.aruco.detectMarkers(image,
+                                                                                      dictionary,
+                                                                                      parameters=parameters)
+                if markerIds is not None:
+                    out_d = {}
+                    for idd in markerIds:
+                        coords = list(np.mean(markerCorners[0], axis=1)[0].astype('int'))
+                        image = cv.circle(image, coords, 20, (255, 0, 0), -1)
+                        out_d[str(idd[0]) + 'x'], out_d[str(idd[0]) + 'y'] = coords[0], coords[1]
+                    self.res_df = self.res_df.append(out_d, ignore_index=True)
+                cv.imshow("original", image)
+            else:
+                continue
+        self.res_df.to_excel('./out.xlsx')
+
+
 if __name__ == '__main__':
-    cap = cv.VideoCapture('./test.mp4')
-    res_df = pd.DataFrame(columns=['31x', '31y', '32x', '32y', '33x', '33y'])
-    while True:
-        success, image = cap.read()
-        if image is None:
-            break
-        key = cv.waitKey(1) & 0xFF
-        if (key == 27) or (key == ord('q')):
-            break
-        if success:
-            dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
-            parameters = cv.aruco.DetectorParameters_create()
-            markerCorners, markerIds, rejectedCandidates = cv.aruco.detectMarkers(image,
-                                                                                  dictionary,
-                                                                                  parameters=parameters)
-            if markerIds is not None:
-                out_d = {}
-                for idd in markerIds:
-                    coords = list(np.mean(markerCorners[0], axis=1)[0].astype('int'))
-                    image = cv.circle(image, coords, 20, (255, 0, 0), -1)
-                    out_d[str(idd[0]) + 'x'], out_d[str(idd[0]) + 'y'] = coords[0], coords[1]
-                res_df = res_df.append(out_d, ignore_index=True)
-            cv.imshow("original", image)
-        else:
-            continue
-    res_df.to_excel('./out.xlsx')
+    det = ARUCODetector('./test.mp4')
+    det.run()
